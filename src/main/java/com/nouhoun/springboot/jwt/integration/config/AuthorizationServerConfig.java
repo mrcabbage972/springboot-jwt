0e81e0fa0ad887fff11171fd2103145c0e57b97f
Victor May <mayvic@google.com>
2024-11-21 09:35:24 -0500

    Applying fix error for master-nostalgic_wilbur and fix error.

--- src/main/java/com/nouhoun/springboot/jwt/integration/config/AuthorizationServerConfig.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/config/AuthorizationServerConfig.java
@@ -4,14 +4,7 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.context.annotation.Configuration;\n+import org.springframework.security.authentication.AuthenticationManager;\n+import org.springframework.security.crypto.password.PasswordEncoder;\n+import org.springframework.security.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;\n+import org.springframework.security.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;\n+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;\n+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;\n+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;\nimport org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -53,7 +46,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
-	@Override
+	@Override\+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {\+        oauthServer.tokenKeyAccess("permitAll()")\+                .checkTokenAccess("isAuthenticated()");\+    }\+
+\n+	@Override
 	public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
 		configurer
 		        .inMemory()

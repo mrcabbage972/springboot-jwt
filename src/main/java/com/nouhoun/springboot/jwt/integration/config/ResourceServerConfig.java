66d0c413f78708c1728e0e98d6f3ce145e373d8e
mrcabbage972 <mayvic@gmail.com>
2025-01-07 21:32:36 +0000

    migration

--- src/main/java/com/nouhoun/springboot/jwt/integration/config/ResourceServerConfig.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/config/ResourceServerConfig.java
@@ -1,40 +1,5 @@
 package com.nouhoun.springboot.jwt.integration.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+public class ResourceServerConfig {
 
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-
-
-@Configuration
-@EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-    @Autowired
-    private ResourceServerTokenServices tokenServices;
-
-    @Value("${security.jwt.resource-ids}")
-    private String resourceIds;
-
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.resourceId(resourceIds).tokenServices(tokenServices);
-    }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-                http
-                .authorizeRequests()
-                        .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
-                        .mvcMatchers("/actuator/**").permitAll()
-                        .mvcMatchers("/api-docs/**").permitAll()
-                        .mvcMatchers("/springjwt/**" ).authenticated()
-                        .and().csrf(AbstractHttpConfigurer::disable);
-    }
 }

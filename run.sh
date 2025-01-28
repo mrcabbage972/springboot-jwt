3ed2342c9119836ef20754857f4ccb392671afbd
mayvic <mayvic@google.com>
2025-01-28 00:48:22 +0000

    WIP

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-docker run -it --rm -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3.8.1-openjdk-17 mvn clean install -DskipTests
+#!/bin/bash\nchmod +x run.sh\ndocker run -it --rm -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3.8.1-openjdk-17 mvn clean install -DskipTests

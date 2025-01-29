84656aa2028e40745a697bf0a3b35dbe34c6d49e
root <root@localhost>
2025-01-29 16:21:47 +0000

    WIP

--- src/main/java/com/nouhoun/springboot/jwt/integration/config/ResourceServerConfig.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/config/ResourceServerConfig.java
@@ -1,35 +1,29 @@
 package com.nouhoun.springboot.jwt.integration.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
-@EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-    @Autowired
-    private ResourceServerTokenServices tokenServices;
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${security.jwt.resource-ids}")
     private String resourceIds;
 
     @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.resourceId(resourceIds).tokenServices(tokenServices);
-    }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-                http
-                .requestMatchers()
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
                 .antMatchers("/actuator/**", "/api-docs/**").permitAll()
-                .antMatchers("/springjwt/**" ).authenticated();
+                .antMatchers("/springjwt/**").authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
     }
 }

36cbe084bc895e4e966a82b5797e2602a4f120f0
root <root@localhost>
2025-01-30 18:00:06 +0000

    WIP

--- src/test/java/com/nouhoun/springboot/jwt/integration/SpringbootJwtApplicationTests.java
+++ src/test/java/com/nouhoun/springboot/jwt/integration/SpringbootJwtApplicationTests.java
@@ -1,16 +1,13 @@
 package com.nouhoun.springboot.jwt.integration;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootJwtApplicationTests {
+class SpringbootJwtApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

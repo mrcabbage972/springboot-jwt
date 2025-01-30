ecb4038308aeb833932841f2950f30d32406a7a6
root <root@localhost>
2025-01-30 18:04:34 +0000

    WIP

--- src/main/resources/sql-scripts/data.sql
+++ src/main/resources/sql-scripts/data.sql
@@ -21,3 +21,10 @@ INSERT INTO random_city(id, name) VALUES (5, 'New York City');
 INSERT INTO random_city(id, name) VALUES (6, 'Mopti');
 INSERT INTO random_city(id, name) VALUES (7, 'Koulikoro');
 INSERT INTO random_city(id, name) VALUES (8, 'Moscow');
+
+INSERT INTO role (id, name) VALUES (1, 'STANDARD_USER');
+
+INSERT INTO users (id, username, email, password, enabled, first_name, last_name, last_password_reset_date)
+VALUES (1, 'user', 'user@example.com', '$2a$04$bzS11/R/yDFHWOxVfj942.t4IhCwIr2qG65Bf1O2BkdR/7hrpThqe', TRUE, 'User', 'User', null);
+
+INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);

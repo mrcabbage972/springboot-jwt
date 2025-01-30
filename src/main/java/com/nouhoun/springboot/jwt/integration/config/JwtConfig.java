86bf12ec8be7838f22336e0df6ea273ea58e5d18
root <root@localhost>
2025-01-30 18:22:39 +0000

    WIP

--- src/main/java/com/nouhoun/springboot/jwt/integration/config/JwtConfig.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/config/JwtConfig.java
@@ -1,7 +1,9 @@
 package com.nouhoun.springboot.jwt.integration.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtConfig {
 
     @Value("${security.jwt.uri:/auth/**}")
